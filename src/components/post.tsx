66f10c07d412e83db3c43d2e5d8e580244986724
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2025-06-07 16:32:39 +0900

    chore: lint

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -2,24 +2,24 @@ import { allPosts } from "content-collections";
 import { notFound } from "next/navigation";
 
 interface PostProps {
-	slug: string;
+  slug: string;
 }
 
 export const Post = ({ slug }: PostProps) => {
-	const post = allPosts.find((post) => post.slug === slug);
-	if (!post) {
-		return notFound();
-	}
+  const post = allPosts.find((post) => post.slug === slug);
+  if (!post) {
+    return notFound();
+  }
 
-	const MdxContent = post.content;
+  const MdxContent = post.content;
 
-	return (
-		<article className="post">
-			<h2>{post.title}</h2>
-			<div className="content">
-				<MdxContent />
-			</div>
-			ㅎㅇ
-		</article>
-	);
+  return (
+    <article className="post">
+      <h2>{post.title}</h2>
+      <div className="content">
+        <MdxContent />
+      </div>
+      ㅎㅇ
+    </article>
+  );
 };
